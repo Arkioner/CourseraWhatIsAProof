be56fdc2aa6388e27427ebdae8ef519355838f57
Rafael Vila <rafael.vila@scmspain.com>
2018-02-12 14:24:38 +0100

    remove useless code

--- src/DiceProject/DiceProject.py
+++ src/DiceProject/DiceProject.py
@@ -16,14 +16,7 @@ def count_wins(dice1, dice2):
 
 def get_winner_dice(dice1, dice2):
     assert len(dice1) == 6 and len(dice2) == 6
-    dice1_wins, dice2_wins = 0, 0
-
-    for pair in list(itertools.product(dice1, dice2)):
-        if pair[0] > pair[1]:
-            dice1_wins += 1
-        elif pair[0] < pair[1]:
-            dice2_wins += 1
-
+    dice1_wins, dice2_wins = count_wins(dice1, dice2)
     comparison = dice2_wins - dice1_wins
     output_map = {0: -1, -1: 0, 1: 1}
     return output_map[(comparison > 0) - (comparison < 0)]
@@ -51,7 +44,7 @@ def map_dice_wins(dices):
     assert all(len(dice) == 6 for dice in dices)
     dices_info = dict()
     for index, item in enumerate(dices):
-        dices_info[index] = {'wins': 0, 'vs': [], 'owned': {'dice': -1, 'by': 0}}
+        dices_info[index] = {'wins': 0, 'owned': {'dice': -1, 'by': 0}}
 
     for pair in list(itertools.combinations(dices_info, 2)):
         win_count = count_wins(
@@ -61,12 +54,10 @@ def map_dice_wins(dices):
         win_difference = abs(win_count[0] - win_count[1])
         if win_count[0] > win_count[1]:
             dices_info[pair[0]]['wins'] += 1
-            dices_info[pair[0]]['vs'].append({'dice': pair[1], 'by': win_difference})
             if dices_info[pair[1]]['owned']['by'] < win_difference:
                 dices_info[pair[1]]['owned'] = {'dice': pair[0], 'by': win_difference}
         if win_count[0] < win_count[1]:
             dices_info[pair[1]]['wins'] += 1
-            dices_info[pair[1]]['vs'].append({'dice': pair[0], 'by': win_difference})
             if dices_info[pair[0]]['owned']['by'] < win_difference:
                 dices_info[pair[0]]['owned'] = {'dice': pair[1], 'by': win_difference}
     return dices_info
